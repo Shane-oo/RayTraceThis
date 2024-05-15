3bb9ed88992699a46175f52134aa402da42e657c
Shane Monck <shane@constructivesoftware.com.au>
2024-05-16 07:52:32 +0800

    Initial Project

---
+++ src/SMCodesRenderEngine/SMCodesRenderEngine/SMCodesRenderEngine.cpp
@@ -0,0 +1,11 @@
+﻿// SMCodesRenderEngine.cpp : Defines the entry point for the application.
+//
+
+#include "SMCodesRenderEngine.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello CMake." << endl;
+    return 0;
+}
